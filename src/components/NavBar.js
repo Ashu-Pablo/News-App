3600167d492bb43ce4d8d65b64846602c3a4a7fb
Om Dixit <omnathdixit7033@gmail.com>
2024-02-28 21:56:35 +0530

    news app updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,7 +8,7 @@ export class navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" >
                     <div className="container-fluid">
-                        <Link className="navbar-brand" to="/">NewsWala</Link>
+                        <Link className="navbar-brand" to="/">HeadlineHub</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
